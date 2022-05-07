f720e83d536f0684547e9d773e6a02027c7bcd62
Ayush Batra <ayushbatra128@gmail.com>
2022-05-07 21:13:45 +0530

    some changes

--- src/components/Global/ProductCard/ProductCard.jsx
+++ src/components/Global/ProductCard/ProductCard.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { ButtonStyled } from "../../../Styled/Button";
 import "./productcard.css";
 
 export const ProductCard = ({ info }) => {
+  const getToken = () => {
+    let token = JSON.parse(localStorage.getItem("Usertoken")) || "";
+    let config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return config;
+  };
+
+  const addtocart = (product_id) => {
+    axios
+      .post(
+        `https://bobbi-brown-api.herokuapp.com/cart/add/${product_id}`,
+        getToken()
+      )
+      .then((res) => console.log(res));
+  };
+
   return (
     <div className="product___card">
       <Link to={`/products/${info.page}/${info.id}`}>
@@ -11,7 +29,14 @@ export const ProductCard = ({ info }) => {
       <span>{info.name}</span>
       <p>{info.tag}</p>
       <span>${info.price}</span>
-      <ButtonStyled size="medium">ADD TO BAG</ButtonStyled>
+      <ButtonStyled
+        size="medium"
+        onClick={() => {
+          addtocart(info.id);
+        }}
+      >
+        ADD TO BAG
+      </ButtonStyled>
     </div>
   );
 };
