438780b0e8024150770c37f5669b22be17345e2b
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-05-09 12:04:25 +0530

    Changed title bar icon

---
+++ src/components/Global/Nav/NavComp/components/Logout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./User.css"
+import { FiX } from "react-icons/fi";
+import { IconContext } from 'react-icons';
+import { BsFacebook } from "react-icons/bs";
+
+
+export default function User() {
+    const crossIcon = () => {
+        document.querySelector(".hiddenBoxofLogin").classList.add("hidden")
+        console.log("cross is clicked")
+    }
+
+    return (
+        <>
+
+        
+
+        </>
+        
+    )
+}
